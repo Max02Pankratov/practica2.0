7d8b1c97a75eb59ffd20deb81a1514c302f75b6b
Max02Pankratov <emailqwer.com@gmail.com>
2023-04-11 16:33:28 +0300

    practika2

---
+++ practica/scripts.js
@@ -0,0 +1,8 @@
+// script.js
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.header__nav');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+  burger.classList.toggle('toggle');
+});
